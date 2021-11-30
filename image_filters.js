daf10546ad22cbfab3d87a074dcfdfd5d05478f6
maryannho123 <69371492+maryannho123@users.noreply.github.com>
2021-11-30 12:09:24 -0500

    ugj

--- image_filters.js
+++ image_filters.js
@@ -1,18 +1,37 @@
-import java.util.Scanner;
-Scanner reader = new Scanner(System.in);  // Reading from System.in
-System.out.println("Enter a color: ");
-char n = reader.nextInt(); // Scans the next token of the input as an int.
-//once finished
-reader.close();
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+function onClick() {
+  if (inputsAreEmpty()) {
+    label.textContent = 'Error: one or both inputs are empty.';
+    return;
+  }
+  updateLabel();
+}
+function inputsAreEmpty() {
+  if (getNumber1() === '' || getNumber2() === '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+function updateLabel() {
+  var addend1 = getNumber1();
+  var addend2 = getNumber2();
+  var sum = addend1 + addend2;
+  label.textContent = addend1 + ' + ' + addend2 + ' = ' + sum;
+}
+function getNumber1() {
+  return inputs[0].value;
+}
+function getNumber2() {
+  return inputs[1].value;
+}
+var inputs = document.querySelectorAll('input');
+var label = document.querySelector('p');
+var button = document.querySelector('button');
+button.addEventListener('click', onClick);
 
-});
 
 
 
-
-
-$("#filter").click(function(){
-    $("img").addClass("sepia");
-});
+// $("#filter").click(function(){
+//     $("img").addClass("sepia");
+// });
