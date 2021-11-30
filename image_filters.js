b96c64c78b4a01e6070a7ca80091514a6786b6a9
maryannho123 <69371492+maryannho123@users.noreply.github.com>
2021-11-30 12:04:21 -0500

    attempting java script

---
+++ image_filters.js
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+Scanner reader = new Scanner(System.in);  // Reading from System.in
+System.out.println("Enter a color: ");
+char n = reader.nextInt(); // Scans the next token of the input as an int.
+//once finished
+reader.close();
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+
+
+
+
+$("#filter").click(function(){
+    $("img").addClass("sepia");
+});
