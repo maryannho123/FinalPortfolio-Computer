f1f4dbd92c2ef5e98868d3643b4b7a6c94a12235
maryannho123 <69371492+maryannho123@users.noreply.github.com>
2021-12-01 13:15:08 -0500

    final css update

--- image_filters.js
+++ image_filters.js
@@ -1,19 +1,80 @@
 
 var button;
+var buttonActive ; 
+var lastClicked;
+lastClicked = $("#undo_button");;
+var somethingClicked = 0; 
+$('#sepia_button').bind('click', function() {
+   buttonActive= $("#sepia_button");
+   buttonActive.toggleClass("active");
+   button = "sepia";
+   if(lastClicked != buttonActive){
+    lastClicked.removeClass("active");
+   }
 
-document.querySelector("#sepia_button").addEventListener("click", function(){
-    button = "sepia";
+   lastClicked = buttonActive;
+//    console.log("Last Clicked" + lastClicked);
+//    console.log("Clicked" + buttonActive);
+});
+$('#blur_button').bind('click', function() {
+   buttonActive= $('#blur_button');
+   buttonActive.toggleClass("active");
+   button = "blur";
+  if(lastClicked != buttonActive){
+    lastClicked.removeClass("active");
+   }
+
+   lastClicked = buttonActive;
+});
+$('#gray_button').bind('click', function() {
+   buttonActive= $('#gray_button');
+   buttonActive.toggleClass("active");
+   button = "gray";
+   if(lastClicked != buttonActive){
+    lastClicked.removeClass("active");
+   }
+
+   lastClicked = buttonActive;
+});
+$('#saturate_button').bind('click', function() {
+   buttonActive= $('#saturate_button');
+   buttonActive.toggleClass("active");
+   button = "saturate";
+    if(lastClicked != buttonActive){
+    lastClicked.removeClass("active");
+   }
+
+   lastClicked = buttonActive;
 });
 
-document.querySelector("#blur_button").addEventListener("click", function() {
-    button = "blur";
+$('#undo_button').bind('click', function() {
+   buttonActive= $('#undo_button');
+   buttonActive.toggleClass("active");
+   button = "undo";
+    if(lastClicked != buttonActive){
+    lastClicked.removeClass("active");
+   }
+
+   lastClicked = buttonActive;
 });
-document.querySelector("#gray_button").addEventListener("click", function() {
-    button = "gray";
+
+$('.button').bind('click', function() {
+  if(lastClicked != buttonActive){
+    lastClicked.removeClass("active");
+  }
+ 
 });
-document.querySelector("#saturate_button").addEventListener("click", function() {
-    button = "saturate";
-});
+
+
+// document.querySelector("#blur_button").addEventListener("click", function() {
+//     button = "blur";
+// });
+// document.querySelector("#gray_button").addEventListener("click", function() {
+//     button = "gray";
+// });
+// document.querySelector("#saturate_button").addEventListener("click", function() {
+//     button = "saturate";
+// });
 
 
 $("#filter1").click(function(){ // image1
