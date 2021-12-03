c7ed635064fc42fea4a9999587338e4f7cdda392
maryannho123 <69371492+maryannho123@users.noreply.github.com>
2021-12-03 14:58:40 -0500

    A lot of button stuff

--- image_filters.js
+++ image_filters.js
@@ -1,9 +1,10 @@
 
-var button;
+var button = "empty";
 var buttonActive ; 
 var lastClicked;
-lastClicked = $("#undo_button");;
+lastClicked = $("#undo_button");
 var somethingClicked = 0; 
+
 $('#sepia_button').bind('click', function() {
    buttonActive= $("#sepia_button");
    buttonActive.toggleClass("active");
@@ -53,6 +54,7 @@ $('#undo_button').bind('click', function() {
    button = "undo";
     if(lastClicked != buttonActive){
     lastClicked.removeClass("active");
+    
    }
 
    lastClicked = buttonActive;
@@ -65,53 +67,117 @@ $('.button').bind('click', function() {
  
 });
 
-
-// document.querySelector("#blur_button").addEventListener("click", function() {
-//     button = "blur";
-// });
-// document.querySelector("#gray_button").addEventListener("click", function() {
-//     button = "gray";
-// });
-// document.querySelector("#saturate_button").addEventListener("click", function() {
-//     button = "saturate";
-// });
-
-
 $("#filter1").click(function(){ // image1
-    $(".image1").addClass(button);
+    $("#image1").addClass(button);
+    if(button == "undo"){
+        $("#image1").removeClass();
+    }
 });
 
 $("#filter2").click(function(){// image2
-    $(".image2").addClass(button);
+    $("#image2").addClass(button);
+    if(button == "undo"){
+        $("#image2").removeClass();
+    }
 });
 
 $("#filter3").click(function(){// image3
-    $(".image3").addClass(button);
+    $("#image3").addClass(button);
+    if(button == "undo"){
+        $("#image3").removeClass();
+    }
 });
 
 $("#filter4").click(function(){// image4
-    $(".image4").addClass(button);
+    $("#image4").addClass(button);
+    if(button == "undo"){
+        $("#image4").removeClass();
+    }
 });
 
 $("#filter5").click(function(){// image5
-    $(".image5").addClass(button);
+    $("#image5").addClass(button);
+    if(button == "undo"){
+        $("#image5").removeClass();
+    }
 });
 
 $("#filter6").click(function(){// image6
-    $(".image6").addClass(button);
+    $("#image6").addClass(button);
+    if(button == "undo"){
+        $("#image6").removeClass();
+    }
 });
 
 $("#filter7").click(function(){// image7
-    $(".image7").addClass(button);
+    $("#image7").addClass(button);
+    if(button == "undo"){
+        $("#image7").removeClass();
+    }
 });
 
 $("#filter8").click(function(){// image8
-	console.log("Pause Video");
+    $("#image8").addClass(button);
+    if(button == "undo"){
+        $("#image8").removeClass();
+    }
 
-    $(".image8").addClass(button);
+    
 });
 
 $("#filter9").click(function(){// image9
-	console.log("Pause Video");
-    $(".image9").addClass(button);
+    $("#image9").addClass(button);
+    if(button == "undo"){
+        $("#image9").removeClass();
+    }
 });
+
+$('#image1').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter1').click();//Trigger search button click event
+    }
+});
+$('#image2').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter2').click();//Trigger search button click event
+    }
+});
+$('#image3').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter3').click();//Trigger search button click event
+    }
+});
+$('#image4').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter4').click();//Trigger search button click event
+    }
+});
+$('#image5').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter5').click();//Trigger search button click event
+    }
+});
+$('#image6').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter6').click();//Trigger search button click event
+    }
+});
+
+$('#image7').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter7').click();//Trigger search button click event
+    }
+});
+
+$('#image8').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter8').click();//Trigger search button click event
+    }
+});
+
+$('#image9').keypress(function(e){
+    if(e.which == 13){//Enter key pressed
+        $('#filter9').click();//Trigger search button click event
+    }
+});
+
